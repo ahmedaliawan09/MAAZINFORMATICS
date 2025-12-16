2e59c8f63b7cdbfbbb32bb4d03a44681d64025d4
ahmedaliawan09 <ahmedawan81151@gmail.com>
2025-12-16 17:26:31 +0500

    created folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div class="text-center mt-12 font-bold text-red-500 text-xl">HEllo MUBEEN</div>
+    </>
+  )
+}
+
+export default App
